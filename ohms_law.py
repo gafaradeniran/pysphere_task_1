e5a00f1b7295f22789cf557c110c5ccd21085abd
Gafar Adeniran <gaphcen4better@gmail.com>
2025-08-06 10:03:04 +0100

    Added solution for Ohm's Law assignment

---
+++ ohms_law.py
@@ -0,0 +1,11 @@
+print("Welcome\nThis program calculates Ohms Law")
+i = float(input("Enter the value of current (I): "))
+r = float(input("Enter the value of resistance (R): "))
+
+try:
+    v = i * r
+    print(f"Your voltage is: {v:.2f} volts")
+    print("Thank you for using this program!")
+    
+except():
+    print('You have entered a non-numerical value, pls try again')
